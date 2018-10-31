32c64893fd60d06e34d14613f0abbee216d62ca6
Girish Patel <patelg123@gmail.com>
2018-10-31 16:50:03 +0000

    initial express-mongodb setup

---
+++ server/models/Fiddle.js
@@ -0,0 +1,66 @@
+const mongoose = require('mongoose');
+
+let FiddleSchema = new mongoose.Schema(
+    {
+      _id:  String,
+      fiddle: String,
+      name: String,
+      description: String,
+      creator: String,
+      private: Integer,
+      stars: Integer,
+      value: String,
+    }
+);
+
+FiddleSchema.methods.create = function() {
+  // create code in here
+
+  return this.save();
+}
+
+FiddleSchema.methods.update = function() {
+  // update code in here
+
+  return this.save();
+}
+
+FiddleSchema.methods.star = function() {
+  this.stars++;
+  return this.save();
+}
+
+FiddleSchema.methods.fork = function() {
+  // fork code here
+
+
+}
+
+
+FiddleSchema.methods.download = function() {
+  // download code here
+
+
+}
+
+FiddleSchema.methods.create_code_block = function() {
+  // create_code_block in here
+
+  return this.save();
+}
+
+
+
+FiddleSchema.methods.recent = function() {
+  // recent code here
+
+
+}
+
+FiddleSchema.methods.popular = function() {
+  // popular code here
+
+
+}
+
+module.exports = mongoose.model('Fiddle', 'FiddleSchema');
