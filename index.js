ff571224e499dee4fdd2fb38ea27fe191c334b63
Girish Patel <patelg123@gmail.com>
2018-11-02 16:36:34 +0000

    Neated up the fiddle.ts router and controller

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-require('dist/index.js');
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+
+app.use('/fiddles', require('./server/routes/fiddle.ts'));
+
+
+
+//const routes = require('../server/routes');
+
+
+//const path = require('path');
+//const http = require("http");
+//require('dist/index.js');
+
+//app.use('/', routes);
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
--- server/routes/fiddle.ts
+++ server/routes/fiddle.ts
@@ -1,8 +1,14 @@
-const fiddlecontroller = require('./../controllers/fiddle.ctrl');
+const express = require('express');
+const router = express.Router();
+const fiddlecontroller = require('./../controllers/fiddle.ts');
 
-module.exports = (router) => {
-  router
-    .route('/fiddles')
-    .get(fiddlecontroller.getFiddles)
+router.post('/', fiddlecontroller.createFiddle);
+router.put('/:fiddleid', fiddlecontroller.updateFiddle);
+router.post('/:fiddleid/star', fiddlecontroller.starFiddle);
+router.post('/:fiddleid/fork', fiddlecontroller.forkFiddle);
+router.post('/:fiddleid/download', fiddlecontroller.downloadFiddle);
+router.post('/:fiddleid/code-blocks', fiddlecontroller.createBlockImageFiddle);
+router.get('/recent', fiddlecontroller.recentFiddles);
+router.get('/popular', fiddlecontroller.popularFiddles);
 
-}
+module.exports = router;
