b3c02ac5f136be9227757f988b964d57d4a41280
Girish Patel <patelg123@gmail.com>
2018-11-02 16:44:46 +0000

    Neated up the fiddle.ts router and controller

--- server/controllers/fiddle.ts
+++ server/controllers/fiddle.ts
@@ -1,10 +1,29 @@
 const Fiddle = require('./../models/Fiddle');
 const User = require('./../models/User');
 
-
 module.exports = {
-  getFiddles: (req, res, next) => {
-    //code to get fiddles
-  }
-
-}
+  createFiddle: (req, res, next) => {
+    res.json({ function: [ { name: 'createFiddle' } ] });
+  },
+  updateFiddle: (req, res, next) => {
+    res.json({ function: [ { name: 'updateFiddle' } ] });
+  },
+  starFiddle: (req, res, next) => {
+    res.json({ function: [ { name: 'starFiddle' } ] });
+  },
+  forkFiddle: (req, res, next) => {
+    res.json({ function: [ { name: 'forkFiddle' } ] });
+  },
+  downloadFiddle: (req, res, next) => {
+    res.json({ function: [ { name: 'downloadFiddle' } ] });
+  },
+  createBlockImageFiddle: (req, res, next) => {
+    res.json({ function: [ { name: 'createBlockImageFiddle' } ] });
+  },
+  recentFiddles: (req, res, next) => {
+    res.json({ function: [ { name: 'recentFiddles' } ] });
+  },
+  popularFiddles: (req, res, next) => {
+    res.json({ function: [ { name: 'popularFiddles' } ] });
+  },
+};
