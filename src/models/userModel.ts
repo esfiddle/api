611830a74d06e0cb0e3c84085f4e95c11082e772
Josh Ghent <me@joshghent.com>
2018-11-22 22:38:33 +0000

    Added start of the user model

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -2,37 +2,55 @@ import * as mongoose from "mongoose";
 
 const Schema = mongoose.Schema;
 
-export const FiddleSchema = new Schema({
+export const IdentitiesSchema = new Schema({
+
+});
+
+export const UserSchema = new Schema({
       _id:  {
         required: true,
         type: String,
       },
-      creator: {
+      email: {
+        required: true,
+        trim: true, unique: true,
+        match: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        type: String,
+      },
+      biography: {
         required: false,
         type: String,
       },
-      description: {
-        required: false,
-        type: String,
-      },
-      fiddle: {
-        required: true,
-        type: String,
-      },
-      name: {
-        required: true,
-        type: String,
-      },
-      private: {
-        required: true,
-        type: Number,
-      },
-      stars: [{
-        _id: String,
-        date: Date,
-      }],
-      value: {
-        required: true,
-        type: String,
+      githubProvider: {
+        type: {
+          id: String,
+          token: String,
+        },
+        select: false,
       },
 });
+
+UserSchema.statics.upsertGithubUser = function(token: string, tokenSecret: string, profile: any, cb: any) {
+  const that = this;
+  return this.findOne({
+    "githubProvider.id": profile.id,
+  }, function(err: any, user: any) {
+    if (!user) {
+      const newUser = new that({
+        email: profile.emails[0].value,
+        githubProvider: {
+          id: profile.id,
+          token,
+          tokenSecret,
+        },
+      });
+
+      newUser.save((error: any, savedUser: any) => {
+        if (error) { console.log(error); }
+        return cb(error, savedUser);
+      });
+    } else {
+      return cb(err, user);
+    }
+  });
+};
