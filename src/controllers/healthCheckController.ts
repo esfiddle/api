a22647b3da68489ef94db43dee4e85833d916157
Derek Willingham <derek.willingham@tinroofsoftware.com>
2019-10-10 23:17:26 -0400

    fix linting errors

--- src/controllers/healthCheckController.ts
+++ src/controllers/healthCheckController.ts
@@ -1,45 +1,45 @@
 import { Request, Response } from "express";
 import * as mongoose from "mongoose";
 
-
-const mongooseConnectionStates = {
+const mongooseConnectionStates: { [key: string]: any } = {
     0: "disconnected",
     1: "connected",
     2: "connecting",
-    3: "disconnecting"
-}
+    3: "disconnecting",
+};
 
 export class HealthCheckController {
 
     public check = (req: Request, res: Response) => {
 
-        let isServerHealthy = true
+        let isServerHealthy = true;
 
-        let mongooseState = mongoose.connection.readyState;
-        if (mongooseState != 1)
-            isServerHealthy = false
+        const mongooseState = mongoose.connection.readyState;
+        if (mongooseState !== 1) {
+            isServerHealthy = false;
+        }
 
         // Add additional checks here
 
-        let healthStatus = {
-          'status': isServerHealthy ? 'healthy' : 'unhealthy',
-          'services': {
-            'db_connection': mongooseConnectionStates[mongooseState]
-          }
-        }
+        const healthStatus = {
+            services: {
+                db_connection: mongooseConnectionStates[mongooseState],
+            },
+            status: isServerHealthy ? "healthy" : "unhealthy",
+        };
 
         if (isServerHealthy) {
             return res
                 .status(200)
-                .type('json')
-                .send(healthStatus)
+                .type("json")
+                .send(healthStatus);
         } else {
             return res
                 .status(500)
-                .type('json')
-                .send(healthStatus)
+                .type("json")
+                .send(healthStatus);
         }
-    };
+    }
 
 }
 
